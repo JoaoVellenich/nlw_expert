9bef720ecaef775fd73ef218d9b162de6a504acc
JoaoVellenich <jpvellenich@gmail.com>
2024-02-10 13:24:09 -0300

    feat: first commit

---
+++ src/main/java/br/com/joaovellenich/certification_nlw/CertificationNlwApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaovellenich.certification_nlw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationNlwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationNlwApplication.class, args);
+	}
+
+}
